7bdfa59324048181d3711a01b7ef36aa1877297c
syahirahhalim <syahawesome@gmail.com>
2022-09-29 14:27:08 +0800

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list.component';
+import { TodoComponent } from './components/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ListComponent, TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
